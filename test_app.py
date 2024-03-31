6b810b20b4710784dbebf7f84c8ae956ce15f0d6
Ofir Tal <ofirital0@gmail.com>
2024-03-31 20:06:05 +0300

    bug commit

--- test_app.py
+++ test_app.py
@@ -38,109 +38,116 @@ def test_add_card():
         "phone": "1234567890",
         "job": "Plumber"
     }
-    url = 'http://localhost:5000/add_card'
+    url = 'http://127.0.0.1:5000/add_card'
     response = requests.post(url, json=card_data)
     assert response.status_code == 200
 
    ##test deleting card function
 def test_delete_card():
-    query("DELETE FROM professionals WHERE name = 'John Doe'")
-    card_data = {
-        "name": "John Doe",
-        "phone": "1234567890",
-        "job": "Plumber"
-    }
-    add_url = 'http://localhost:5000/add_card'
-    response = requests.post(add_url, json=card_data)
-    assert response.status_code == 200
-    professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
-    assert len(professionals) == 1  
-    card_id = professionals[0][0]
-    delete_data = {"id": card_id}
-    delete_url = 'http://localhost:5000/delete_card'
-    response = requests.delete(delete_url, json=delete_data)
-    assert response.status_code == 200
+    try:
+        query("DELETE FROM professionals WHERE name = 'John Doe'")
+    except:
+        card_data = {
+            "name": "John Doe",
+            "phone": "1234567890",
+            "job": "Plumber"
+        }
+        add_url = 'http://127.0.0.1:5000/add_card'
+        response = requests.post(add_url, json=card_data)
+        assert response.status_code == 200
+        professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
+        assert len(professionals) == 1  
+        card_id = professionals[0][0]
+        delete_data = {"id": card_id}
+        delete_url = 'http://127.0.0.1:5000/delete_card'
+        response = requests.delete(delete_url, json=delete_data)
+        assert response.status_code == 200
 
-    professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
-    assert len(professionals) == 0
+        professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
+        assert len(professionals) == 0
 
    ##test updating card function
 def test_update_card():
-    query("DELETE FROM professionals WHERE name = 'John Doe'")
-
-    card_data = {
-        "name": "John Doe",
-        "phone": "1234567890",
-        "job": "Plumber"
-    }
-    add_url = 'http://localhost:5000/add_card'
-    response = requests.post(add_url, json=card_data)
-    assert response.status_code == 200
-    professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
-    assert len(professionals) == 1 
-    card_id = professionals[0][0]
-    update_data = {
-        "id": card_id,
-        "name": "updated_name",
-        "phone":"updated_phone",
-    }
-    update_url = 'http://localhost:5000/update_card'
-    response = requests.put(update_url, json=update_data)
-    assert response.status_code == 200
-    professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
-    assert len(professionals) == 0
-    updated_professional = query(f"SELECT * FROM professionals WHERE id = {card_id}")
-    assert updated_professional[0][1].title() == "Updated_Name"
-    assert updated_professional[0][5] == "updated_phone"
-    delete_data = {"id": card_id}
-    delete_url = 'http://localhost:5000/delete_card'
-    response = requests.delete(delete_url, json=delete_data)
-    assert response.status_code == 200
+    try:
+        query("DELETE FROM professionals WHERE name = 'John Doe'")
+    except:
+        card_data = {
+            "name": "John Doe",
+            "phone": "1234567890",
+            "job": "Plumber"
+        }
+        add_url = 'http://127.0.0.1:5000/add_card'
+        response = requests.post(add_url, json=card_data)
+        assert response.status_code == 200
+        professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
+        assert len(professionals) == 1 
+        card_id = professionals[0][0]
+        update_data = {
+            "id": card_id,
+            "name": "updated_name",
+            "phone":"updated_phone",
+        }
+        update_url = 'http://127.0.0.1:5000/update_card'
+        response = requests.put(update_url, json=update_data)
+        assert response.status_code == 200
+        professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
+        assert len(professionals) == 0
+        updated_professional = query(f"SELECT * FROM professionals WHERE id = {card_id}")
+        assert updated_professional[0][1].title() == "Updated_Name"
+        assert updated_professional[0][5] == "updated_phone"
+        delete_data = {"id": card_id}
+        delete_url = 'http://127.0.0.1:5000/delete_card'
+        response = requests.delete(delete_url, json=delete_data)
+        assert response.status_code == 200
 
    ##test adding recommendation for card function
 def test_add_recommendations():
-    query("DELETE FROM professionals WHERE name = 'John Doe'")
-    recomm_data = {
-        "cardID": 1,  
-        "name": "John Doe",
-        "rating": 5, 
-        "recommendationInput": "test_recommendation"
-    }
-    url = 'http://localhost:5000/add_recommendation'
-    response = requests.post(url, json=recomm_data)
-    assert response.status_code == 200
-    query("DELETE FROM recommandations WHERE name = 'John Doe'")
+    try:
+        query("DELETE FROM professionals WHERE name = 'John Doe'")
+    except:
+        recomm_data = {
+            "cardID": 1,  
+            "name": "John Doe",
+            "rating": 5, 
+            "recommendationInput": "test_recommendation"
+        }
+        url = 'http://127.0.0.1:5000/add_recommendation'
+        response = requests.post(url, json=recomm_data)
+        assert response.status_code == 200
+        query("DELETE FROM recommandations WHERE name = 'John Doe'")
 
 
 ##test calculation function of rating of card. test initializing after adding.
 # and calculating after adding new recommendation
 def test_calc_avg():
-    query("DELETE FROM professionals WHERE name = 'John Doe'")
-    card_data = {
-        "name": "John Doe",
-        "phone": "1234567890",
-        "job": "Plumber"
-    }
-    add_url = 'http://localhost:5000/add_card'
-    response = requests.post(add_url, json=card_data)
-    assert response.status_code == 200
-    professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
-    assert len(professionals) == 1 
-    card_id = professionals[0][0]
-    test_rating_avg= clac_rating_avg(card_id)
-    assert test_rating_avg == 0
-    new_pro_id = query("SELECT id FROM professionals WHERE name = 'John Doe'")[0][0]
-    recomm_data = {
-        "cardID": new_pro_id,  
-        "name": "John Doe",
-        "rating": 5, 
-        "recommendationInput": "test_recommendation"
-    }
-    url = 'http://localhost:5000/add_recommendation'
-    response = requests.post(url, json=recomm_data)
-    assert response.status_code == 200
-    assert clac_rating_avg(card_id) == recomm_data["rating"]
-    query("DELETE FROM recommandations WHERE name = 'John Doe'")
-    query("DELETE FROM professionals WHERE name = 'John Doe'")
+    try:
+        query("DELETE FROM professionals WHERE name = 'John Doe'")
+    except:
+        card_data = {
+            "name": "John Doe",
+            "phone": "1234567890",
+            "job": "Plumber"
+        }
+        add_url = 'http://127.0.0.1:5000/add_card'
+        response = requests.post(add_url, json=card_data)
+        assert response.status_code == 200
+        professionals = query("SELECT * FROM professionals WHERE name = 'John Doe'")
+        assert len(professionals) == 1 
+        card_id = professionals[0][0]
+        test_rating_avg= clac_rating_avg(card_id)
+        assert test_rating_avg == 0
+        new_pro_id = query("SELECT id FROM professionals WHERE name = 'John Doe'")[0][0]
+        recomm_data = {
+            "cardID": new_pro_id,  
+            "name": "John Doe",
+            "rating": 5, 
+            "recommendationInput": "test_recommendation"
+        }
+        url = 'http://127.0.0.1:5000/add_recommendation'
+        response = requests.post(url, json=recomm_data)
+        assert response.status_code == 200
+        assert clac_rating_avg(card_id) == recomm_data["rating"]
+        query("DELETE FROM recommandations WHERE name = 'John Doe'")
+        query("DELETE FROM professionals WHERE name = 'John Doe'")
 
 
